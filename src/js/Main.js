a3959de4b79fa6f1790361bcf26bd22a87f12724
Siarhei Maksimau1 <siarhei_maksimau1@epam.com>
2018-11-28 19:09:31 +0300

    fixing prod build

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,12 +1,13 @@
 //import Articles from './services/ArticlesService.js';
 import Sources from './services/SourcesService.js';
 import '../css/main.less';
+__webpack_public_path__ = "public/js/";
 
 const sources = new Sources();
 sources.getAllSouces();
 
 document.getElementById('apply-filters').addEventListener("click", () => {
-  import('./services/ArticlesService.js').then(module => {
+  import(/* webpackChunkName: "ArticlesService" */'./services/ArticlesService.js').then(module => {
     console.log('articles loaded');
     let articles = new module.default();
     articles.applyFilters();
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,11 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
+    devtool: 'inline-source-map',
     plugins: [
-        new CleanWebpackPlugin(['public']),
-        new HtmlWebpackPlugin({
-            title: 'Production'
-        })
+        new CleanWebpackPlugin(['public/js/'])
     ]
 });
